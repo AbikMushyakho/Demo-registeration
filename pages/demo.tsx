493b5c4d21c9abb46610be7e9efd48cf13c95f53
AbikMushyakho <abikmushyakho94@gmail.com>
2022-11-15 21:57:28 +0545

    CRUD operation complete using mock api and axios

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,6 +1,8 @@
+import axios from "axios";
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Demo.module.css";
+import { ToastContainer, toast } from "react-toastify";
 
 type AppProps = {
   message: string;
@@ -17,6 +19,18 @@ export default function Home({ message }: AppProps): JSX.Element {
   const [userList, setUserList] = useState<UserList>([] as UserList);
   const [user, setUser] = useState<User>({ name: "", age: 0 } as User);
 
+  const fetchUser = async (): Promise<void> => {
+    const response = await axios.get(
+      "https://636b387dc07d8f936db0905c.mockapi.io/api/guest"
+    );
+    if (response.status === 200) {
+      setUserList(response.data);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   const handleOnChange = (event: React.FormEvent<HTMLInputElement>): void => {
     const target = event.target as HTMLTextAreaElement;
     setUser({
@@ -24,33 +38,75 @@ export default function Home({ message }: AppProps): JSX.Element {
       [target?.name]: target?.value,
     });
   };
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault();
 
     const exist = userList.some((u) => u.name === user.name);
     if (exist) {
-      return alert("User Already exists!");
+      if (confirm("User Already exists! Do you want to update??") === true) {
+        const toUpdate: UserList = userList.filter((u) => u.name === user.name);
+        const updateData = {
+          id: toUpdate[0].id,
+          name: user.name,
+          age: user.age,
+        };
+        const updated = await axios.put(
+          `https://636b387dc07d8f936db0905c.mockapi.io/api/guest/${toUpdate[0].id}`,
+          updateData
+        );
+        if (updated.status === 200) {
+          toast.success("User updated successfully...");
+          const updatedUserList: UserList = userList.map((u) =>
+            u.id !== updateData.id ? u : updateData
+          );
+          setUserList(updatedUserList);
+          setUser({ name: "", age: 0 });
+        }
+      }
     } else if (user.name === "" || user.age === 0) {
-      return alert("please enter your name and age!");
+      alert("please enter your name and age!");
     } else {
       const userWithId = {
         id: userList.length + 1,
         ...user,
       };
-      setUserList([...userList, userWithId]);
-      setUser({ name: "", age: 0 });
+
+      const postedData = await axios.post(
+        "https://636b387dc07d8f936db0905c.mockapi.io/api/guest",
+        userWithId
+      );
+      if (postedData.status === 201) {
+        setUserList([...userList, userWithId]);
+        setUser({ name: "", age: 0 });
+        toast.success("New data added successfully");
+      }
     }
   };
 
-  const handleDeleteClick = (user: {
+  const handleDeleteClick = async (user: {
     id: number;
     name: string;
     age: number;
-  }): void => {
+  }): Promise<void> => {
     if (confirm(`Are you sure you want to delete ${user.name}`) === true) {
-      const deletedList = userList.filter((list) => list.id !== user.id);
-      setUserList(deletedList);
+      const del = await deleteUser(user.id);
+      if (del === true) {
+        const deletedList = userList.filter((list) => list.id !== user.id);
+        setUserList(deletedList);
+        toast.success("Deleted successfully..");
+      }
     }
+  };
+  const deleteUser = async (id: number): Promise<Boolean> => {
+    const del = await axios.delete(
+      `https://636b387dc07d8f936db0905c.mockapi.io/api/guest/${id}`
+    );
+    if (del?.status === 200) {
+      return true;
+    }
+    return false;
   };
   return (
     <div>
@@ -58,7 +114,7 @@ export default function Home({ message }: AppProps): JSX.Element {
         <title>Register</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <ToastContainer />
       <main className={styles.main}>
         <form
           className={styles.form}
@@ -92,11 +148,15 @@ export default function Home({ message }: AppProps): JSX.Element {
             {userList.length > 0 ? (
               userList.map((user, index) => {
                 return (
-                  <li key={index} className={styles.listItem}>
+                  <li
+                    key={index}
+                    className={styles.listItem}
+                    onClick={(e) => setUser({ name: user.name, age: user.age })}
+                  >
                     <div>
-                     <span>
-                     {user.name} of age {user.age}
-                     </span>
+                      <span>
+                        {user.name} of age {user.age}
+                      </span>
                       <img
                         onClick={(e) => handleDeleteClick(user)}
                         width="20px"
