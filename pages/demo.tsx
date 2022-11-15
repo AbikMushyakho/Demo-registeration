0791843e02a616c7d6262adfc697a600520dffaa
AbikMushyakho <abikmushyakho94@gmail.com>
2022-11-15 20:38:26 +0545

    delete added

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React, { useState } from "react";
 import styles from "../styles/Demo.module.css";
 
@@ -10,16 +11,18 @@ interface User {
   age: number;
 }
 
-type UserList = Array<{ name: string; age: number }>;
+type UserList = Array<{ id: number; name: string; age: number }>;
 
 export default function Home({ message }: AppProps): JSX.Element {
-  const [user, setUser] = useState<User>({ name: "", age: 0 } as User);
-
   const [userList, setUserList] = useState<UserList>([] as UserList);
+  const [user, setUser] = useState<User>({ name: "", age: 0 } as User);
 
   const handleOnChange = (event: React.FormEvent<HTMLInputElement>): void => {
     const target = event.target as HTMLTextAreaElement;
-    setUser({ ...user, [target?.name]: target?.value });
+    setUser({
+      ...user,
+      [target?.name]: target?.value,
+    });
   };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
@@ -30,50 +33,86 @@ export default function Home({ message }: AppProps): JSX.Element {
     } else if (user.name === "" || user.age === 0) {
       return alert("please enter your name and age!");
     } else {
-      setUserList([...userList, user]);
+      const userWithId = {
+        id: userList.length + 1,
+        ...user,
+      };
+      setUserList([...userList, userWithId]);
       setUser({ name: "", age: 0 });
     }
   };
+
+  const handleDeleteClick = (user: {
+    id: number;
+    name: string;
+    age: number;
+  }): void => {
+    if (confirm(`Are you sure you want to delete ${user.name}`) === true) {
+      const deletedList = userList.filter((list) => list.id !== user.id);
+      setUserList(deletedList);
+    }
+  };
   return (
-    <main className={styles.main}>
-      <form className={styles.form} onSubmit={handleSubmit} autoComplete="off">
-        <h2>Register</h2>
-        <label className={styles.label}>Name</label>
-        <input
-          className={styles.input}
-          name="name"
-          type="text"
-          value={user?.name}
-          onChange={handleOnChange}
-        />
-        <label className={styles.label}>Age</label>
-        <input
-          className={styles.input}
-          name="age"
-          type="number"
-          value={user?.age}
-          onChange={handleOnChange}
-        />
-        <button className={styles.button} type="submit">
-          Register
-        </button>
-      </form>
-      <div className={styles.listHead}>
-        <h3 className={styles.listTitle}>Registered Users</h3>
-        <ul className={styles.unOrdered}>
-          {userList.length > 0 ? (
-            userList.map((user,index) => {
-              return (
-                <li key={index} className={styles.listItem}>
-                  {user.name} of age {user.age}
-                </li>
-              );
-            })
-          ) : (
-            <li className={styles.listItem}>No any user registered!</li>
-          )}
-        </ul>
-      </div>
-    </main>
+    <div>
+      <Head>
+        <title>Register</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit}
+          autoComplete="off"
+        >
+          <h2>Register</h2>
+          <label className={styles.label}>Name</label>
+          <input
+            className={styles.input}
+            name="name"
+            type="text"
+            value={user?.name}
+            onChange={handleOnChange}
+          />
+          <label className={styles.label}>Age</label>
+          <input
+            className={styles.input}
+            name="age"
+            type="number"
+            value={user?.age}
+            onChange={handleOnChange}
+          />
+          <button className={styles.button} type="submit">
+            Register
+          </button>
+        </form>
+        <div className={styles.listHead}>
+          <h3 className={styles.listTitle}>Registered Users</h3>
+          <ul className={styles.unOrdered}>
+            {userList.length > 0 ? (
+              userList.map((user, index) => {
+                return (
+                  <li key={index} className={styles.listItem}>
+                    <div>
+                     <span>
+                     {user.name} of age {user.age}
+                     </span>
+                      <img
+                        onClick={(e) => handleDeleteClick(user)}
+                        width="20px"
+                        height="20px"
+                        src="https://img.icons8.com/color/48/null/delete-forever.png"
+                      />
+                    </div>
+                  </li>
+                );
+              })
+            ) : (
+              <li className={styles.listItem}>No any user registered!</li>
+            )}
+          </ul>
+        </div>
+      </main>
+    </div>
   );
 }
